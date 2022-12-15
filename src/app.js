80609b080211d7b62df330cd9d456a240df163c2
IgnacioMuela101122 <igna8@hotmail.com>
2022-12-15 12:46:47 -0300

    adaptado para render

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const path = require ('path');
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log('Server running in http:/localhost:3000'));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log('Server running in http:/localhost:' + port));
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 
